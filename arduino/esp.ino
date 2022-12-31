c8835590a7ace8f4694c4acb532de5f3cdcbb230
Shaladdin <abyboyganteng@gmail.com>
2022-12-31 07:08:58 +0700

    code brrr

--- arduino/esp.ino
+++ arduino/esp.ino
@@ -2,25 +2,26 @@
 #include <SPI.h>
 #include <Wire.h>
 #include <MFRC522.h>
-// #include <type_traits>
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 #include <ArduinoJson.h>
 #include <Adafruit_MLX90614.h>
 #include <LiquidCrystal_I2C.h>
+#include "header.h"
 
 // RFID Variables
-#define resetPin D3
-#define ssPin D8
-MFRC522 Reader;
-String lastCard;
-//unsigned long int lastReading;
+// #define resetPin D3
+// #define ssPin D8
+// MFRC522 Reader;
+// String lastCard;
+// unsigned long int lastReading;
 
-
-void setup(){
-
+void setup()
+{
+    WebsocketInit();
 }
 
-void loop(){
-
+void loop()
+{
+    WebsocketRun();
 }
---
+++ arduino/header.h
@@ -0,0 +1,61 @@
+// Copied from MRC
+
+#include <Arduino.h>
+#include <ArduinoJson.h>
+// #include <SoftwareSerial.h>
+#include <ESP8266WiFi.h>
+#include <ArduinoWebsockets.h>
+using namespace websockets;
+
+// // outputs pin
+// #define lamp D1
+// #define fan D2
+// #define buzzer D0
+// const int outputs[] = {lamp, fan, buzzer};
+
+// // ir
+// #define irDalam D6
+// #define irLuar D5
+// const int inputs[] = {irDalam, irLuar};
+
+// void pinInit();
+
+// Websocket
+void sendWs(String &in);
+void WebsocketInit();
+void WebsocketRun();
+extern bool activated;
+
+// // Security
+// extern bool securityMode;
+// extern volatile int pendingNotif;
+// void SecurityRun();
+
+// // room detection
+// extern volatile int jumlahOrang;
+// extern volatile bool emptyRoom;
+// extern bool notifSended;
+// void RoomDetectionInit();
+// void RoomDetectionRun();
+
+// // Software serial
+// extern SoftwareSerial nano;
+// extern bool connectedToNano;
+// void SerialInit();
+// void SerialRun();
+
+// // Sensor Data:
+
+// // sensors reading
+// extern float lpg, co, smoke,
+//     temp, humidity, hIndex,
+//     flame, light;
+
+// // StreamSensor
+// void StreamSensors();
+
+// // smartHome
+// extern bool controllMode;
+// extern float maxBright, maxFlame, maxTemp, maxGas;
+// void SmartHomeRun();
+// void ManualRun();
